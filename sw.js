ce8339b8e544dc617a222dddb999b13788a30c45
progfay <progfay@gmail.com>
2024-06-19 14:24:09 +0900

    `prettier -w`

--- sw.js
+++ sw.js
@@ -1,49 +1,50 @@
-const CACHE_NAME = 'qr-web-share-target'
+const CACHE_NAME = "qr-web-share-target";
 
 const SAME_ORIGIN_RESOURCE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/sw.js',
-  '/qrcode.min.js',
-  '/favicon.ico',
-  '/images/android-chrome-192x192.png',
-  '/images/android-chrome-512x512.png',
-  '/images/apple-touch-icon.png',
-  '/images/favicon-16x16.png',
-  '/images/favicon-32x32.png'
-]
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/sw.js",
+  "/qrcode.min.js",
+  "/favicon.ico",
+  "/images/android-chrome-192x192.png",
+  "/images/android-chrome-512x512.png",
+  "/images/apple-touch-icon.png",
+  "/images/favicon-16x16.png",
+  "/images/favicon-32x32.png",
+];
 
 const CROSS_ORIGIN_RESOURCE = [
-  'https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js'
-]
+  "https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js",
+];
 
-self.addEventListener('install', e => {
+self.addEventListener("install", (e) => {
   const installing = async () => {
-    const cache = await caches.open(CACHE_NAME)
+    const cache = await caches.open(CACHE_NAME);
     return cache.addAll([
       ...SAME_ORIGIN_RESOURCE,
-      ...CROSS_ORIGIN_RESOURCE.map(url => new Request(url, { mode: 'cors' }))
-    ])
-  }
+      ...CROSS_ORIGIN_RESOURCE.map((url) => new Request(url, { mode: "cors" })),
+    ]);
+  };
 
-  e.waitUntil(installing())
-})
+  e.waitUntil(installing());
+});
 
-self.addEventListener('fetch', function (e) {
+self.addEventListener("fetch", function (e) {
   e.respondWith(
-    caches.match(e.request.url, { ignoreSearch: true })
-    .then(async response => {
-      if (response) return response
+    caches
+      .match(e.request.url, { ignoreSearch: true })
+      .then(async (response) => {
+        if (response) return response;
 
-      const resp = fetch(e.request.url, { mode: 'cors' })
+        const resp = fetch(e.request.url, { mode: "cors" });
 
-      setTimeout(async () => {
-        const cache = await caches.open(CACHE_NAME)
-        await cache.put(e.request.url, resp)
-      }, 0)
+        setTimeout(async () => {
+          const cache = await caches.open(CACHE_NAME);
+          await cache.put(e.request.url, resp);
+        }, 0);
 
-      return response
-    })
-  )
-})
+        return response;
+      }),
+  );
+});
